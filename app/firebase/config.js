3812828edadef9e2e182add6dd06f60812db9bcd
Craigen1 <113487759+Craigen1@users.noreply.github.com>
2024-01-05 22:24:06 +0800

    initialize firebase

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -11,7 +11,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
